cca94288136454c41ecf642777e788e5ae46387c
Adsskk19 <saikiranankam19@gmail.com>
2026-04-22 12:30:33 +0530

    singleton

--- TestCodes/src/com/singleton/Single.java
+++ TestCodes/src/com/singleton/Single.java
@@ -1,8 +1,18 @@
 package com.singleton;
 
 public class Single {
-	
+	private static Single obj;
 	private Single() {
+		
+	}
+	
+	private static Single getSingletonClass() {
+		if(obj==null) {
+			obj = new Single();
+		}
+		return obj;
+		
+		
 		
 	}
 	
